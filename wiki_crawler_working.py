e9e5d492edd7e1a2ca49b73effc1c38f60749660
trxw <okhosravani@gmail.com>
2014-03-29 16:25:14 -0400

    Update wiki_crawler_working.py

--- wiki_crawler_working.py
+++ wiki_crawler_working.py
@@ -1,205 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf_8 
 #Note: In order to use urllib2 python 2.6+ is required.
+import sys
 import urllib
 import urllib2
 from collections import Counter
-
-
-
-##################For further development####################:
-# How to detect a location page like http://en.wikipedia.org/wiki/Princeton,_New_Jersey
-# How to detect publishers and news agencies such as http://en.wikipedia.org/wiki/Scientific_American
-# and http://en.wikipedia.org/wiki/BBC
-# http://en.wikipedia.org/wiki/Wikipedia:Biographies_of_living_persons
-# http://en.wikipedia.org/w/index.php?title=Category:1960_births&from=K
-# http://en.wikipedia.org/wiki/Wikipedia:Who_is_a_low_profile_individual
-# http://en.wikipedia.org/wiki/Category:Living_people
-# http://www.crummy.com/software/BeautifulSoup/bs4/doc/#installing-a-parser
-# requests: an alternative to urllib2 which is used by everyone nowedays according to themselves:
-# http://docs.python-requests.org/en/latest/
-# For requests also see:
-# http://stackoverflow.com/questions/10342694/sending-requests-to-webpages-using-pythons-urllib2
-# Log into with python:
-# http://stackoverflow.com/questions/189555/how-to-use-python-to-login-to-a-webpage-and-retrieve-cookies-for-later-usage
-# Avoid non html content in request:
-# http://stackoverflow.com/questions/8479736/using-python-urllib-how-to-avoid-non-html-content
-
-
-
+from LinkStorage import UNDESIRABLE_LINKS, TEMPLATE_OR_CATEGORY, FILES, BAD_EXTENTIONS, BOOK_SITE_LIST
 
 
 #Set the parent link where crawling begins:
 url = "http://en.wikipedia.org/wiki/Mathematics"
 # Undesirable links:
-UNDESIRABLE_LINKS = [ 
-	'/w/index.php?title=event_(' # These two are to exclude possible  
-	################################ direct self-referral since it creates
-	################################ an infinite loop while crawling the
-	################################ links referred in the html data.
-	'//creativecommons.org/licenses/by-sa/3.0/',
-	'/wiki/category:wikipedia_articles_needing',
-	'/wiki/category:pages_containing_cite_templates',
-	'/wiki/category:featured_articles',
-	'/wiki/category:wikipedia_semi-protected_pages',
-	'//wikimediafoundation.org/wiki/privacy_policy',
-	'/wiki/main_page',
-	'//en.wikipedia.org/wiki/wikipedia:contact_us',
-	'/wiki/wikipedia:about',
-	'/wiki/help:',
-	'/wiki/help:',
-	'//shop.wikimedia.org',
-	'/wiki/special:recentchangeslinked',
-	'/wiki/special:booksources/',
-	'/wiki/wikipedia:community_portal',
-	'/wiki/wikipedia:community_portal',
-	'/wiki/category:all_articles_needing_additional_references',
-	'//meta.wikimedia.org',
-	'/wiki/category:all_articles_lacking_sources',
-	'//be-x-old.wikipedia.org/wiki/',
-	'//wikimediafoundation.org/wiki/terms_of_use',
-	'https://www.mediawiki.org/wiki/special:mylanguage/how_to_contribute',
-	'/wiki/Scientific_American',
-	'/wiki/Portal:',
-	'/wiki/Scientific_American',
-	'/wiki/portal:featured_content',
-	'/wiki/portal:current_events',
-	'/wiki/help:category',
-	'/wiki/file:question_book-new.svg',
-	'/wiki/wikipedia:file_upload_wizard',
-	'/w/index.php?title=special:recentchanges&amp;feed=atom',
-	'#p-search',
-	'#see_also',
-	'/wiki/special:random',
-	'/wiki/wikipedia:citing_sources',
-	'/wiki/special:recentchanges',
-	'//bits.wikimedia.org/apple-touch/wikipedia.png',
-	'/wiki/help:introduction_to_referencing/1',
-	'/wiki/category:articles_lacking_sources_from_july_2007',
-	'//en.wikipedia.org/w/api.php?action=rsd',
-	'//www.mediawiki.org/',
-	'/wiki/category:wikipedia_indefinitely_move-protected_pages',
-	'//en.wikipedia.org/wiki/wikipedia:',
-	'https://donate.wikimedia.org/wiki/special:',
-	'http://en.wikipedia.org/wiki/template_talk:',
-	'/wiki/template_talk:',
-	'/wiki/category:articles_with_open_directory_project_links',
-	'//en.wikipedia.org/w/index.php?title=template:',
-	'http://www.wikidata.org/wiki/',## Note that a link that begins with 
-	################################### this refers to the corresponding
-	################################### page in which the equivalent of 
-	################################### the title of the current page in 
-	################################### other languages are listed.
-	'//www.wikidata.org/wiki/',
-	'/wiki/special:specialpages',
-	'/wiki/talk:',
-	'//bits.wikimedia.org/en.wikipedia.org/load',
-	'/wiki/portal:contents',
-	'/w/opensearch_desc.php',
-	'//bits.wikimedia.org/favicon/wikipedia.ico',
-	'//bits.wikimedia.org',
-	'/w/index.php?title=special:book&',
-	'/wiki/special:whatlinkshere',
-	'/w/index.php?title=special:userlogin',
-	'/wiki/oxford_university_press',
-	'/wiki/wikipedia:',
-	'/wiki/especial:',
-	'/wiki/especial%',
-	'/wiki/spezial:',
-	'/wiki/spezial%',
-	'/wiki/special:',
-	'/wiki/special%',
-	'/wiki/wikipedia_diskussion:',
-	'/wiki/spesial:',
-	'/wiki/spesial%',
-	'/wiki/specjalna:',
-	'/wiki/specjalna%',
-	'/wiki/speciaal:',
-	'/wiki/speciaal%',
-	'/wiki/speciel:',
-	'/wiki/speciel%',
-	'/wiki/category:cs1_errors:_dates',
-	'/wiki/category:hidden_templates_using_styles',
-	'/wiki/category:wikipedia_indefinitely_semi-protected_pages',
-	'/wiki/category:all_articles_with_unsourced_statements',
-	'/wiki/category:articles_with_unsourced_statements',
-	'/wiki/book:',
-	'/w/index.php?title=',
-	'#mw-navigation',
-	'/wiki/category:articles_containing',
-	'/wiki/category:use_dmy_dates_from',
-	'/wiki/Digital_object_identifier',
-	'/wiki/Bibcode',
-	]
-
-# Note that before checking this list, it must be ensured that the link
-# is not a bad link, that is it is not in either UNDESIRABLE_LINKS, FILES
-# or in BAD_EXTENTIONS.
-
-
-## IMPORTANT NOTE: This can in fact help a lot in determining important
-	##################### subcatagories of each field. For example, /wiki/Category:Mathematics
-	##################### has a subcaragory /wiki/Category:Fields_of_Mathematics 
-TEMPLATE_OR_CATEGORY = [
-	'/wiki/template:',
-	'/wiki/category:', 
-	'/template:', 
-	]
-
-FILES = [
-	'/wiki/file:',
-	'.jpg',
-#	'.JPG', 
-	'.png',
-#	'.PNG', 
-	'.pdf',
-#	'.PDF',
-	'.svg',
-	]	
-
-BAD_EXTENTIONS = [
-	'edit',
-	'printable=yes',
-	'#sitelinks-wikipedia',
-	'#mw-navigation',
-	'wikipedia:general_disclaimer',
-	'&amp;action=info',
-	'skin=vector&amp;*',
-	'&amp;writer=rl',
-	'#external_links',
-	'#p-search',
-	'#a_note_on_notation',
-	'#external_links',
-	'#notes',
-	'#see_also',
-	'/wiki/inverse_image',
-	'/w/index.',
-	'wikipedia.',
-	'wikipedia.',
-	'wikipedia',
-	'/ka.wikipedia.',
-	'pedia.',
-	'wikimediafoundation.org/',
-	'wikimediafoundation.org',
-	'/wiki/springer_science%2bbusiness_media',
-	'http://en.wikipedia.org/wiki/OCLC',
-	'//en.wikipedia.org/wiki/OCLC',
-	#'mwl',  	
-	#'vec',
-	#'csb'
-	#'zea'
-	#'bat-smg'
-	'/wiki/international_standard_book_number',
-	'education.aspx',
-	'/w/',
-	'/trap/',
-	]
-
-BOOK_SITE_LIST = [
-	"books.google.com/",
-	"www.amazon.",
-	]
-
 
 
 def extract_link(data, start_link, end_link):
@@ -471,10 +282,98 @@ class Link(object):  #='NOPARENTLINK'  default parent?
 	def __repr__(self):
 		return self.get_link()
 
+  
+  
+  
 
-def get_links(data, given_url):
-	url = str(given_url)
-	end_link = 0
+def main():
+  
+  url = Link("http://en.wikipedia.org/wiki/Albert_Einstein")
+  
+  # html_data = url.get_html()
+  dics_list = get_links(url)
+  
+  print url.get_wikipedia_page_topic() + "\n"
+  
+  for i in range(len(dics_list)):
+    print "There are " + str(len(dics_list[i])) + " links.\n"
+  
+  
+  print "There are "+str(len(dics_list[1])) + " unique external links on the page."
+  
+  
+  t = sort_dic(dics_list[5])
+  
+  f = open(url.get_actual_link()[url.get_actual_link().find('wiki/')+5 : ]+".txt", "w")
+  
+  wikipedia_links_sorted = sort_dic(dics_list[5])
+  number_of_wikipedia_links = len(wikipedia_links_sorted)
+  f.write("There are " + str(number_of_wikipedia_links) + " links in dic_wikipedia_links: \n") 
+  
+  for i in range(len(wikipedia_links_sorted)):
+    f.write(str(wikipedia_links_sorted[i][0]) + " : " + str(wikipedia_links_sorted[i][1]) + "\n")   
+  
+  #for k in dics_list[5]:
+  #   f.write(str(k) + " : " + str(dics_list[5][k]) + "\n")    
+  
+  q = sort_dic(dics_list[6])
+  f.write("\n\n\n\n\n\n")
+  f.write("Ranked dic_wikipedia_person_links are: \n")  
+  for j in range(len(q)):
+    f.write(str(q[j][0]) + " : " + str(q[j][1]) + "\n")    
+  
+  
+  
+  f.write("\n\n\n\n\n\n")
+  
+  ## Write the dic_external_links:
+  f.write("Ranked dic_wikipedia_links are: \n") 
+  for i in range(len(t)):
+    f.write(str(t[i][0]) + " : " + str(t[i][1]) + "\n")
+  
+  f.write("\n\n\n\n\n\n")
+  
+  f.write("dic_internal_links are: \n") 
+  for j in dics_list[0]:
+    f.write(str(j) + " : " + str(dics_list[0][j]) + "\n")    
+  
+  f.write("\n\n\n\n\n\n")
+  
+  f.write("dic_wikipedia_non_en_links are: \n")    
+  for k in dics_list[2]:
+    f.write(str(k) + " : " + str(dics_list[2][k]) + "\n")    
+  
+  f.write("\n\n\n\n\n\n")
+  
+  f.write("dic_cite_numbers are: \n")   
+  for k in dics_list[3]:
+    f.write(str(k) + " : " + str(dics_list[3][k]) + "\n")    
+  
+  f.write("\n\n\n\n\n\n")
+  
+  f.write("dic_book_links are: \n") 
+  for k in dics_list[4]:
+    f.write(str(k) + " : " + str(dics_list[4][k]) + "\n")    
+  
+  f.write("\n\n\n\n\n\n")
+  
+  f.write("dic_wikipedia_template_or_category_links are: \n") 
+  for k in dics_list[7]:
+    f.write(str(k) + " : " + str(dics_list[7][k]) + "\n")    
+  
+  
+  f.close() 
+  
+  print "\n"
+  print len(dics_list[7])
+
+
+
+def get_links(url):
+    
+  #url = str(given_url)
+ 	# url = "http://en.wikipedia.org/wiki/Adolf_Hitler" 
+	# end_link = 0
 	#li=[]
 	dic_internal_links = {}
 	dic_external_links = {}
@@ -484,136 +383,64 @@ def get_links(data, given_url):
 	dic_wikipedia_links = {}
 	dic_wikipedia_person_links = {}
 	dic_wikipedia_template_or_category_links = {}
-	link_count = 0
-	start_link = 0
+	#link_count = 0
+	#start_link = 0
 
 	#test_link = Link('/wiki/template:')
-
-	while start_link != -1:
+	for current_link in link_gen(url):
+    #	while start_link != -1:
 		
 		# Note that getStart_link() and getEnd_link will both return -1 
 		# if there are no more hrefs:
-		start_link = get_start_link(data, end_link) 
-		end_link = get_end_link(data, start_link)
+		#start_link = get_start_link(data, end_link) 
+		#end_link = get_end_link(data, start_link)
 	
-		if start_link != -1:
-			link_count += 1
+		#if start_link != -1:
+		#	link_count += 1
 			## Extract the link as string:      
-			current_link = Link(extract_link(data, start_link, end_link), url)
-			
+		#	current_link = Link(extract_link(data, start_link, end_link), url)
+		#current_link = link  		
 			## and Store the extracted link inside list li:     
 			#   li.append(current_link)
 
 			## and Store the extracted link inside dictionary dic, and add 1 to the number of 
 			## occurrence of the link:
-			if current_link.is_good_link(): #len(current_link.get_actual_link()) > 2:
-				if current_link.is_wikipedia_link():     
-					if current_link.is_wikipedia_template_or_category_link():
-						store_link(dic_wikipedia_template_or_category_links, current_link.get_actual_link())
-   					#
-   					elif current_link.is_wikipedia_person():
-						store_link(dic_wikipedia_person_links, current_link.get_actual_link())
-					#
-					else:
-   						store_link(dic_wikipedia_links, current_link.get_actual_link())
-					
-					#else:	
-				elif current_link.is_wikipedia_non_en_link():
-					store_link(dic_wikipedia_non_en_links, current_link.get_actual_link())
+		if current_link.is_good_link(): #len(current_link.get_actual_link()) > 2:
+			if current_link.is_wikipedia_link():     
+				if current_link.is_wikipedia_template_or_category_link():
+					store_link(dic_wikipedia_template_or_category_links, current_link.get_actual_link())
+                    #elif current_link.is_wikipedia_person():
+			    	#	store_link(dic_wikipedia_person_links, current_link.get_actual_link())
+	  	  		else:
+        	    			store_link(dic_wikipedia_links, current_link.get_actual_link())
 
-				elif current_link.is_good_internal_link():
-					if current_link.is_good_cite_number():
-						store_link(dic_cite_numbers, current_link.get_actual_link())
-					else: 
-						store_link(dic_internal_links, current_link.get_actual_link())
+        		elif current_link.is_wikipedia_non_en_link():
+        	  		store_link(dic_wikipedia_non_en_links, current_link.get_actual_link())
+
+			elif current_link.is_good_internal_link():
+				if current_link.is_good_cite_number():
+					store_link(dic_cite_numbers, current_link.get_actual_link())
+				else: 
+					store_link(dic_internal_links, current_link.get_actual_link())
   
-	
-				elif current_link.is_book_link():
-					store_link(dic_book_links, current_link.get_actual_link())
+			elif current_link.is_book_link():
+				store_link(dic_book_links, current_link.get_actual_link())
 				
-				
-				else:                           
-					store_link(dic_external_links, current_link.get_actual_link())
+			else:                           
+				store_link(dic_external_links, current_link.get_actual_link())
 	
 
 	return [dic_internal_links, dic_external_links, dic_wikipedia_non_en_links, dic_cite_numbers, dic_book_links, dic_wikipedia_links, dic_wikipedia_person_links, dic_wikipedia_template_or_category_links]    
 
-url = Link("http://en.wikipedia.org/wiki/Albert_Einstein")
+	
+def link_gen(url):
 
-html_data = url.get_html()
-dics_list = get_links(html_data, url)
+	html = urllib2.urlopen(url.get_actual_link()).read() 
+	start, end = 0, 0 
+	while start != -1:
+		yield Link(html[start+6:end], url)
+		start = html.find('href="', end)
+		end = html.find('"', start+6 )
 
-print url.get_wikipedia_page_topic() + "\n"
-
-for i in range(len(dics_list)):
-	print "There are " + str(len(dics_list[i])) + " links.\n"
-
-
-print "There are "+str(len(dics_list[1])) + " unique external links on the page."
-
-
-t = sort_dic(dics_list[5])
-
-f = open(url.get_actual_link()[url.get_actual_link().find('wiki/')+5 : ]+".txt", "w")
-
-wikipedia_links_sorted = sort_dic(dics_list[5])
-number_of_wikipedia_links = len(wikipedia_links_sorted)
-f.write("There are " + str(number_of_wikipedia_links) + " links in dic_wikipedia_links: \n") 
-
-for i in range(len(wikipedia_links_sorted)):
-	f.write(str(wikipedia_links_sorted[i][0]) + " : " + str(wikipedia_links_sorted[i][1]) + "\n")   
-
-#for k in dics_list[5]:
-#   f.write(str(k) + " : " + str(dics_list[5][k]) + "\n")    
-
-q = sort_dic(dics_list[6])
-f.write("\n\n\n\n\n\n")
-f.write("Ranked dic_wikipedia_person_links are: \n")  
-for j in range(len(q)):
-	f.write(str(q[j][0]) + " : " + str(q[j][1]) + "\n")    
-
-
-
-f.write("\n\n\n\n\n\n")
-
-## Write the dic_external_links:
-f.write("Ranked dic_wikipedia_links are: \n") 
-for i in range(len(t)):
-	f.write(str(t[i][0]) + " : " + str(t[i][1]) + "\n")
-
-f.write("\n\n\n\n\n\n")
-
-f.write("dic_internal_links are: \n") 
-for j in dics_list[0]:
-	f.write(str(j) + " : " + str(dics_list[0][j]) + "\n")    
-
-f.write("\n\n\n\n\n\n")
-
-f.write("dic_wikipedia_non_en_links are: \n")    
-for k in dics_list[2]:
-	f.write(str(k) + " : " + str(dics_list[2][k]) + "\n")    
-
-f.write("\n\n\n\n\n\n")
-
-f.write("dic_cite_numbers are: \n")   
-for k in dics_list[3]:
-	f.write(str(k) + " : " + str(dics_list[3][k]) + "\n")    
-
-f.write("\n\n\n\n\n\n")
-
-f.write("dic_book_links are: \n") 
-for k in dics_list[4]:
-	f.write(str(k) + " : " + str(dics_list[4][k]) + "\n")    
-
-f.write("\n\n\n\n\n\n")
-
-f.write("dic_wikipedia_template_or_category_links are: \n") 
-for k in dics_list[7]:
-	f.write(str(k) + " : " + str(dics_list[7][k]) + "\n")    
-
-
-f.close() 
-
-print "\n"
-print len(dics_list[7])
+if __name__ == "__main__": main()
 
