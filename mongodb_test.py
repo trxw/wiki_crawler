a7f85b47ab1419f995337cf635740a1b273647c2
trxw <okhosravani@gmail.com>
2014-03-29 15:19:43 -0400

    Delete mongodb_test.py

--- mongodb_test.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import crawl_working_1
-from pymongo import MongoClient
-
-client = MongoClient()
-test_database = client['test_database']
-test_collection = test_database['test_collection']
